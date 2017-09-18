d66168fd63019341342ac1864b903b770fade7d3
Mustafa Abdulrazzaq <abmu16@nti.umea>
2017-09-18 10:55:32 +0200

    Skriver Windows 10

---
+++ ovning1_8.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+/*
+	Skriver wINDOWS 10 
+	Mustafa Abdulrazzaq
+	9/18/2017
+*/
+public class ovning1_8 {
+	public static void main(String[] args) {
+	
+	JOptionPane.showMessageDialog(null, "Du kör " +
+		
+		System.getProperty("os.name") + "!");	
+	}
+}
