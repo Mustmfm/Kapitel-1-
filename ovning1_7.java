4beceab1885b37817241118aa77b85a2945fcd39
Mustafa Abdulrazzaq <abmu16@nti.umea>
2017-09-18 10:40:27 +0200

    Ett program som gör ljud

---
+++ ovning1_7.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+	import java.awt.*;
+/*
+Programmet ska göra ljud 
+Mustafa Abdulrazzaq
+9/18/2017
+*/
+public class ovning1_7 {
+	public static void main(String[] args) {
+		
+	
+	Toolkit.getDefaultToolkit() .beep() ;
+	}
+}
