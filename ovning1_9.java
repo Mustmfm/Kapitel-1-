cc2fce1c7233c326b1b185f8217835b7dc77ef55
Mustafa Abdulrazzaq <abmu16@nti.umea>
2017-09-18 11:06:52 +0200

    Programet ska vissa min namn, address och mobilnummer

---
+++ ovning1_9.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+/*
+	Programet ska skriva mitt namn, mi adress och telefonnummer 
+	Mustafa Abdulrazzaq
+	9/18/2017
+*/
+	public class ovning1_9 {
+		public static void main(String[] args) {
+			JOptionPane.showMessageDialog(null,"Mustafa" +
+				"\n Kyrkogatan 10 Nordmaling" +
+				"\n 0703836656");
+		}
+	}
